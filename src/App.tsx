c0a0a8acc6af9c62e25b76ffad80bb07f7728b83
Kuba Flis <jakub.flis@performance-media>
2024-05-06 14:30:16 +0200

    first configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+          <p>NORD SECURITY | Recuritment task</p>
+        <nav>
+          <li><a href="">/login</a></li>
+          {/*<li><a href=""></a></li>*/}
+        </nav>
       </header>
     </div>
   );
