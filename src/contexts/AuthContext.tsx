fdf07dea2fd4cddc260d4e3f03101c0209c03634
Kuba Flis <jakub.flis@performance-media>
2024-05-09 22:03:28 +0200

    create localStorage Context to keep user log in after page refresh

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
-// src/contexts/AuthContext.tsx
 import type { ReactNode } from 'react';
+import { useEffect } from 'react';
 import React, { createContext, useContext, useState } from 'react';
 
 interface AuthContextType {
@@ -12,14 +12,25 @@ interface AuthContextType {
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [token, setToken] = useState<string | null>(null);
 
+  useEffect(() => {
+    const storedToken = localStorage.getItem('authToken');
+    if (storedToken) {
+      setToken(storedToken);
+      setIsAuthenticated(true);
+    }
+  }, []);
+
   const signIn = (newToken: string) => {
+    localStorage.setItem('authToken', newToken);
     setToken(newToken);
     setIsAuthenticated(true);
   };
+
   const logout = () => {
+    localStorage.removeItem('authToken');
     setToken(null);
     setIsAuthenticated(false);
   };
